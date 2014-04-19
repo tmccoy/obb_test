8919ca9548735a9dafff2dd67f09c12bc3153b59
Tyler McCoy <tyler@tyfedora.pbcorp.photobucket.com>
2014-04-19 09:30:39 -0600

    added logic to find table and first entity

--- obb_take_home.py
+++ obb_take_home.py
@@ -7,10 +7,19 @@ parser = OptionParser(usage)
 parser.add_option("-s", action="store", dest="search_string", help="string to narrow search results")
 parser.add_option("-v", action="store_true", dest="verbose", help="run in verbose mode")
 
-url = "http://www.secretary.state.nc.us/corporations/searchresults.aspx?onlyactive=OFF&Words=STARTING&searchstr="
-search_string = "aa"
+url = "http://www.secretary.state.nc.us/corporations/searchresults.aspx?onlyactive=ON&Words=STARTING&searchstr="
+search_string = "AAAAA"
 url_with_search_string = url + search_string
 
 page = urllib2.urlopen(url_with_search_string)
 page_html = page.read()
-#parse = BeautifulSoup(open(page_html))
+parse = BeautifulSoup(page_html)
+
+table = parse.find('table', {'id': "SosContent_SosContent_dgCorps"})
+
+print table
+
+print "______________"
+entity = table.find_all('td')
+
+print entity
